b4c2e1c4492816b2556462788b5e5586e970244b
Veerapandian9698 <veerapandian9698@gmail.com>
2021-12-13 13:49:03 +0530

    generated random value

--- src/app/shared/components/floor-plan/floor-plan.component.ts
+++ src/app/shared/components/floor-plan/floor-plan.component.ts
@@ -10,9 +10,31 @@ export class FloorPlanComponent implements OnInit {
   @Input() img: any;
 
   constructor() { }
+  
 
   ngOnInit(): void {
     console.log(this.img,"image")
+  }
+
+  getColor(value) {
+    if(Math.floor(value) > 26) {
+      return '#ff004f';
+    }
+    else if(Math.floor(value) >= 19 && Math.floor(value) < 21) {
+      return '#0083fd';
+    }
+    else if(Math.floor(value) >= 21 && Math.floor(value) < 23) {
+      return '#00840A';
+    }
+    else if(Math.floor(value) >= 23 && Math.floor(value) < 25) {
+      return '#ffff34';
+    }
+    else if(Math.floor(value) >= 25 && Math.floor(value) <= 26) {
+      return '#ffa500';
+    }
+    else {
+      return '#ffc0cb';
+    }
   }
 
 }
